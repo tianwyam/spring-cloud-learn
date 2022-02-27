d1cbeb32ae8d0cabc3245f5ea19d1c296d287213
tianwyam <1129064096@qq.com>
2022-02-27 14:53:24 +0800

    搭建zuul网关路由

--- spring-cloud-learn-zuul/src/main/java/com/tianya/springcloud/zuul/LearnZuulApplication.java
+++ spring-cloud-learn-zuul/src/main/java/com/tianya/springcloud/zuul/LearnZuulApplication.java
@@ -1,10 +1,19 @@
 package com.tianya.springcloud.zuul;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+// 开启网关路由代理
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
 public class LearnZuulApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(LearnZuulApplication.class, args);
     }
 }
+
